a0e2eb15476279db17fac7e4cce678ebefcbd4ee
Tanmay/K and A Tech - MERN <tanmaykandatech-mern@users-MacBook-Pro.local>
2024-09-16 11:02:26 +0530

    api change

--- client/Api/ApiConstant.js
+++ client/Api/ApiConstant.js
@@ -1,9 +1,9 @@
-// import { backendApiUrl } from "../config";
+import { backendApiUrl } from "../config";
 
 
-const authApiUrl = `/api/v1/auth/user`
-const mainApiUrl  = `/api/v1`
-const postApiUrl = `/api/v1/user/post`
+const authApiUrl = `${backendApiUrl}/api/v1/auth/user`
+const mainApiUrl  = `${backendApiUrl}/api/v1`
+const postApiUrl = `${backendApiUrl}/api/v1/user/post`
 // const authApiUrl = `${backendApiUrl}/auth/user`
 
 export const ApiURL ={
